833c780a3c3695b87c4ce273ebe8708bcfe59f8c
Eray <77186648+Eraycak@users.noreply.github.com>
2022-07-02 19:28:14 +0300

    Joystick is added and player can get money objects

--- NoopeGamesTestCase/Assets/Scripts/MoneyCollector.cs
+++ NoopeGamesTestCase/Assets/Scripts/MoneyCollector.cs
@@ -8,7 +8,7 @@ public class MoneyCollector : MonoBehaviour
     [Header("Ref")]
     public Transform ItemHolderTransform;
     private int NumOfItemsHolding = 0;
-    private float distanceBetweenMoneyObjectsOnHolder = 0.065f;
+    private float distanceBetweenMoneyObjectsOnHolder = -0.015f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,12 +18,12 @@ public class MoneyCollector : MonoBehaviour
 
     public void AddNewItem(Transform itemToAdd)
     {
-        itemToAdd.DOJump(ItemHolderTransform.position + new Vector3(0, distanceBetweenMoneyObjectsOnHolder * NumOfItemsHolding, 0), 1.5f, 1, 0.3f).OnComplete(
+        itemToAdd.DOJump(ItemHolderTransform.position + new Vector3(distanceBetweenMoneyObjectsOnHolder * NumOfItemsHolding, 0, 0), 1.5f, 1, 0.5f).OnComplete(
             () =>
             {
                 itemToAdd.SetParent(ItemHolderTransform, true);
-                itemToAdd.localPosition = new Vector3(0, distanceBetweenMoneyObjectsOnHolder * NumOfItemsHolding, 0);
-                itemToAdd.localRotation = Quaternion.identity;
+                itemToAdd.localPosition = new Vector3(distanceBetweenMoneyObjectsOnHolder * NumOfItemsHolding, 0, -0.0125f);
+                itemToAdd.localRotation = Quaternion.Euler(180f, 90f, 90f);
                 NumOfItemsHolding++;
             }
         );
