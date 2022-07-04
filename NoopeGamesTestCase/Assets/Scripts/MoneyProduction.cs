f71c80510485dd0988001d6fd4f458116e6d7f14
Eray <77186648+Eraycak@users.noreply.github.com>
2022-07-04 23:20:53 +0300

    Building Money Walls

--- NoopeGamesTestCase/Assets/Scripts/MoneyProduction.cs
+++ NoopeGamesTestCase/Assets/Scripts/MoneyProduction.cs
@@ -10,34 +10,34 @@ public class MoneyProduction : MonoBehaviour
     private GameObject createdObject;
     public List<GameObject> moneyObjectsList = new List<GameObject>();
     [SerializeField]
-    private float firstMoneyPosX = -4f, firstMoneyPosY = -0.15f, firstMoneyPosZ = -4f, distanceBetweenMoneyObjsX = 0.92f, distanceBetweenMoneyObjsZ = 1.25f;
+    private float firstMoneyPosX = -4f, firstMoneyPosY = -0.15f, firstMoneyPosZ = -4f, distanceBetweenMoneyObjsX = 0.92f, distanceBetweenMoneyObjsY = 0.33f, distanceBetweenMoneyObjsZ = 1.25f;
     [SerializeField]
     private float timeBetweenProduction = 2f;
     [SerializeField]
-    private int productionAreaLengthX = 10, productionAreaLengthZ = 7;
+    private int productionAreaLengthX = 10, productionAreaLengthZ = 7, productionAreaHeight = 5;
     public bool pauseProduction = false;
     private bool areaIsPurchased = false;
 
     public void AreaIsPurchased()
     {
         areaIsPurchased = true;
-        StartCoroutine(produceMoney(0, 0));
+        StartCoroutine(produceMoney(0, 0, 0));
     }
     public void IsAreaFull()
     {
         if (moneyObjectsList.Count < 70 && areaIsPurchased)
         {
-            //Debug.LogError("isarefull " + moneyObjectsList.Count);
             int i = moneyObjectsList.Count % 10;
             int j = moneyObjectsList.Count / 10;
-            StartCoroutine(produceMoney(i, j));
+            int k = moneyObjectsList.Count / (productionAreaLengthX * productionAreaLengthZ);
+            StartCoroutine(produceMoney(i, j, k));
         }
     }
-    private IEnumerator produceMoney(int i, int j)
+    private IEnumerator produceMoney(int i, int j, int k)
     {
         if (!pauseProduction)
         {
-            Vector3 location = new Vector3(firstMoneyPosX + (distanceBetweenMoneyObjsX * i), firstMoneyPosY, firstMoneyPosZ + (distanceBetweenMoneyObjsZ * j));
+            Vector3 location = new Vector3(firstMoneyPosX + (distanceBetweenMoneyObjsX * i), firstMoneyPosY + (distanceBetweenMoneyObjsY * k), firstMoneyPosZ + (distanceBetweenMoneyObjsZ * j));
             Vector3 buildingLocation = transform.parent.position;
             i++;
             if (i == productionAreaLengthX)
@@ -52,11 +52,15 @@ public class MoneyProduction : MonoBehaviour
             createdObject.GetComponent<SphereCollider>().isTrigger = false;
             createdObject.transform.DOLocalJump(location, 1.5f, 1, 0.5f);
             moneyObjectsList.Add(createdObject);
-            //Debug.LogError("coro " + moneyObjectsList.Count);
-            if (j < productionAreaLengthZ)
+            if (j == productionAreaLengthZ)
+            {
+                j = 0;
+                k++;
+            }
+            if (k<=productionAreaHeight)
             {
                 yield return new WaitForSeconds(timeBetweenProduction);
-                StartCoroutine(produceMoney(i, j));
+                StartCoroutine(produceMoney(i, j, k));
             }
         }
         else
